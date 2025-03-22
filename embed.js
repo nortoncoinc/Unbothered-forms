58c0c38e1fadda55fcb35c8a946912a0c9c7885e
nortoncoinc <79033426+nortoncoinc@users.noreply.github.com>
2025-03-21 22:21:43 -0600

    Update embed.js

--- embed.js
+++ embed.js
@@ -3,7 +3,7 @@
   const script = document.currentScript;
   const formId = script.getAttribute("data-form-id");
   const container = document.createElement("div");
-  fetch(`https://forms.unbotheredmedia.com/forms/${formId}.html`)
+  fetch(`https://unbothered-forms.netlify.app/forms/${formId}.html`)
     .then(res => res.text())
     .then(html => {
       container.innerHTML = html;
